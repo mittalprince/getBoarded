4d4a60389dfc031fadc066dcbeb1862cc70d2402
mittalprince <princemittal766@gmail.com>
2019-01-28 18:15:08 +0530

    Add Job Route

--- pages/jobs.vue
+++ pages/jobs.vue
@@ -1,3 +1,89 @@
 <template> 
-<h2> Jobs </h2>
+    <b-container>
+        <b-row id="profiles">
+                    <b-card-group deck class="mb-3">
+                        <b-card title="Content Writing"
+                                img-src="https://picsum.photos/600/300/?image=25"
+                                img-alt="Image"
+                                img-top
+                                tag="article"
+                                style="max-width: 20rem;"
+                                class="mb-2">
+                            <p class="card-text">
+                            New Delhi, Delhi <br>
+                            M.Tech
+                            </p>
+                            <b-button href="#" variant="primary">Show Profileß</b-button>
+                        </b-card>
+                        <b-card title="Content Writing"
+                                img-src="https://picsum.photos/600/300/?image=25"
+                                img-alt="Image"
+                                img-top
+                                tag="article"
+                                style="max-width: 20rem;"
+                                class="mb-2">
+                            <p class="card-text">
+                            New Delhi, Delhi <br>
+                            M.Tech
+                            </p>
+                            <b-button href="#" variant="primary">Show Profileß</b-button>
+                        </b-card>
+                        <b-card title="Content Writing"
+                                img-src="https://picsum.photos/600/300/?image=25"
+                                img-alt="Image"
+                                img-top
+                                tag="article"
+                                style="max-width: 20rem;"
+                                class="mb-2">
+                            <p class="card-text">
+                            New Delhi, Delhi <br>
+                            M.Tech
+                            </p>
+                            <b-button href="#" variant="primary">Show Profileß</b-button>
+                        </b-card>
+                    </b-card-group>
+                    <b-card-group deck class="mb-3">
+                        <b-card title="Content Writing"
+                                img-src="https://picsum.photos/600/300/?image=25"
+                                img-alt="Image"
+                                img-top
+                                tag="article"
+                                style="max-width: 20rem;"
+                                class="mb-2">
+                            <p class="card-text">
+                            New Delhi, Delhi <br>
+                            M.Tech
+                            </p>
+                            <b-button href="#" variant="primary">Show Profileß</b-button>
+                        </b-card>
+                        <b-card title="Content Writing"
+                                img-src="https://picsum.photos/600/300/?image=25"
+                                img-alt="Image"
+                                img-top
+                                tag="article"
+                                style="max-width: 20rem;"
+                                class="mb-2">
+                            <p class="card-text">
+                            New Delhi, Delhi <br>
+                            M.Tech
+                            </p>
+                            <b-button href="#" variant="primary">Show Profileß</b-button>
+                        </b-card>
+                        <b-card title="Content Writing"
+                                img-src="https://picsum.photos/600/300/?image=25"
+                                img-alt="Image"
+                                img-top
+                                tag="article"
+                                style="max-width: 20rem;"
+                                class="mb-2">
+                            <p class="card-text">
+                            New Delhi, Delhi <br>
+                            M.Tech
+                            </p>
+                            <b-button href="#" variant="primary">Job Profileß</b-button>
+                        </b-card>
+                    </b-card-group>                    
+                    
+                </b-row>
+    </b-container>
 </template>
